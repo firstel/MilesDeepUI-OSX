2b39cb2b7a5ee5e701708e4243f806b660922aa4
Scott Zerby <12087328+firstel@users.noreply.github.com>
2017-12-04 09:04:28 -0600

    Add files via upload

---
+++ miles_deepTests.swift
@@ -0,0 +1,36 @@
+//
+//  miles_deepTests.swift
+//  miles_deepTests
+//
+//  Created by Ketan Kanjiya on 27/11/17.
+//  Copyright © 2017 f(x). All rights reserved.
+//
+
+import XCTest
+@testable import miles_deep
+
+class miles_deepTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
